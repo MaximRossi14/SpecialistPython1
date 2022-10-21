6a92fd9ed0487ea721eeec1885e203767fbfe20d
MaximRossi14 <smk-93@mail.ru>
2022-10-21 15:47:12 +0300

    home_work_04
    ✔

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,20 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+
+import random
+numbers = []
+new_numbers = []
+spisok = []
+n = int(input())
+for i in range(n):
+    numbers.append(random.randint(-10, 30))
+    if numbers[i] >= 0:
+        new_numbers.append(numbers[i]**0.5)
+
+for i in range(len(new_numbers)):
+    if new_numbers[i] % 1 == 0:
+        spisok.append(int(new_numbers[i]))
+print(numbers)
+print(spisok)
